953fb26cf2f0fb1639cb439cd07e328ff3e3ede6
whoagi <whoagir@yandex.ru>
2024-11-21 00:30:51 +0300

    Update 0.0.1a
    Играбельная версия, но надо допилить

--- 0.0.1.py
+++ 0.0.1.py
@@ -7,9 +7,10 @@ player_width = 20
 player_height = 100
 
 # GAME_SCORE
-speed_g = 100
+speed_g = 30
 speed_b = 100
-speed_p = 100
+speed_p1 = 30
+speed_p2 = 10
 
 # COLOR_CONST
 color_white = "white"
@@ -20,18 +21,21 @@ color_green = "green"
 
 
 class GameObject:  # Ну такой общий класс, что бы было
-    def __init__(self, start_point: tuple[int, int], end_point: tuple[int, int], velocity: tuple[int, int],
-                 color=color_cyan):
+    def __init__(self, start_point: tuple[float, float], end_point: tuple[float, float], velocity: tuple[float, float],
+                 speed_: int, color_=color_cyan):
+        print(speed_)
         self.rect = [start_point, end_point]  # начальная точка "ректа"
         self.velocity = velocity  # вектор скорости изменения координат объекта
+        self.color = color_
+        self.speed_ = speed_
         self.update_()  # начальное вычисление центра
-        self.color = color
 
     def update_(self) -> None:  # обновляем позицию по скорости
-        self.rect[0] = (self.rect[0][0] + self.velocity[0] * (speed_g / 100),
-                        self.rect[0][1] + self.velocity[1] * (speed_g / 100))
-        self.rect[1] = (self.rect[1][0] + self.velocity[0] * (speed_g / 100),
-                        self.rect[1][1] + self.velocity[1] * (speed_g / 100))
+        global speed_g
+        self.rect[0] = (self.rect[0][0] + self.velocity[0] * (self.speed_ / 100),
+                        self.rect[0][1] + self.velocity[1] * (self.speed_ / 100))
+        self.rect[1] = (self.rect[1][0] + self.velocity[0] * (self.speed_ / 100),
+                        self.rect[1][1] + self.velocity[1] * (self.speed_ / 100))
         self.center = (self.rect[0][0] + (self.rect[1][0] - self.rect[0][0]) / 2,
                        self.rect[0][1] + (self.rect[1][1] - self.rect[0][1]) / 2)
 
@@ -43,8 +47,9 @@ class GameObject:  # Ну такой общий класс, что бы было
 
 
 class BallObject(GameObject):  # шарик я как и ты был на цепи
-    def __init__(self, start_point: tuple[int, int], end_point: tuple[int, int], velocity: tuple[int, int]):
-        super().__init__(start_point, end_point, velocity)
+    def __init__(self, start_point: tuple[float, float], end_point: tuple[float, float], velocity: tuple[float, float],
+                 speed_):
+        super().__init__(start_point, end_point, velocity, speed_)
         self.radius = ball_radius
 
     def draw(self, turtle) -> None:
@@ -54,9 +59,10 @@ class BallObject(GameObject):  # шарик я как и ты был на цеп
         turtle.dot(self.radius)
 
 
-class Players(GameObject):  # игроки
-    def update_(self) -> None:
-        super().update_()
+class Players(GameObject):  # Игроки
+    def __init__(self, start_point, end_point, velocity, speed_, color_):
+        # Вызываем инициализацию родительского класса GameObject
+        super().__init__(start_point, end_point, velocity, speed_, color_)
         self.velocity = 0, self.velocity[1]
 
     def draw(self, turtle):
@@ -70,11 +76,9 @@ class Players(GameObject):  # игроки
         turtle.goto(self.rect[0][0], self.rect[0][1])
 
     def set_velocity_up(self):
-        print(1)
         self.set_velocity((0, 1))
 
     def set_velocity_down(self):
-        print(2)
         self.set_velocity((0, -1))
 
 
@@ -151,14 +155,17 @@ class Score(UI_Object):
 
 
 class Game(object):
+    global speed_g
+
     def __init__(self):
-        self.player1 = Players((-330, 0), (player_width - 330, player_height), (0, 0), color=color_yellow)
-        self.player2 = Players((330, 0), (player_width + 330, player_height), (0, 2), color=color_green)
+        self.player1 = Players((-330, 0), (player_width - 330, player_height), (0, 0), speed_p1, color_=color_yellow)
+        self.player2 = Players((330, 0), (player_width + 330, player_height), (0, 1), speed_p2, color_=color_green)
         self.border = Border(((-400, 300), (400, 300), (400, -300), (-400, -300)))
         self.score = Score((0, 0))
         self.screen = Screen()
         self.turtle = Turtle()
         self.window = True
+        self.i = 0
 
     def start_game(self):
         self.screen.bgcolor(color_black)
@@ -172,13 +179,17 @@ class Game(object):
         self.objects = (self.ball, self.player2, self.player1, self.border, self.score)
 
     def spawn_new_ball(self):
-        vx = random.choice([x for x in range(-2, 3) if x != 0])  # выбираем случайное значение vx, исключая 0
+        global speed_g
+        speed_g = 30
+        self.i = 0
+        vx = random.choice([x for x in range(-1, 2) if x != 0])  # выбираем случайное значение vx, исключая 0
         vy = random.random() * random.randint(-2, 2)
-        vx, vy = (-0.675, 1)
-        self.ball = BallObject((ball_radius * -1, 0), (ball_radius, ball_radius * 2), (vx, vy))
+        # vx, vy = (-0.675, 1)
+        self.ball = BallObject((ball_radius * -1, 0), (ball_radius, ball_radius * 2), (vx, vy), speed_g)
         self.update_objects()
 
     def run(self):
+        global speed_g
         while self.window:
             if max(self.score.score) >= 9:
                 self.turtle.clear()
@@ -186,10 +197,13 @@ class Game(object):
                 self.turtle.goto((0, 0))
                 self.turtle.down()
                 if self.score.score[0] == 9:
-                    self.turtle.write("win player 2", align="center", font=("Arial", 60, "normal"))
+                    self.turtle.write("win computer you're loser", align="center", font=("Arial", 60, "normal"))
                 else:
                     self.turtle.write("win player 1", align="center", font=("Arial", 60, "normal"))
             else:
+                self.i += 1
+                if self.i % 50 == 0:
+                    speed_g += 1
                 self.turtle.clear()  # отрисовка экрана
 
                 self.check_collision_ball_and_border()
@@ -197,9 +211,12 @@ class Game(object):
                 self.check_collision_player_1_and_ball()
                 self.check_collision_player_and_border(self.player1)
                 self.check_collision_player_and_border(self.player2)
+                self.check_collision_player_2_and_ball()
 
                 self.screen.onkey(self.player1.set_velocity_up, "Up")
                 self.screen.onkey(self.player1.set_velocity_down, "Down")
+
+                self.player2_move()
 
                 for obj in self.objects:
                     obj.update_()
@@ -219,6 +236,29 @@ class Game(object):
                 self.player1.rect[0][0] <= self.ball.center[0] <= self.player1.rect[1][0]):  # x0 <= xb <= x1
             self.ball.set_velocity((self.ball.velocity[0], -self.ball.velocity[1]))
 
+    def check_collision_player_2_and_ball(self):
+        if (self.player2.rect[0][0] + ball_radius / 2 <= self.ball.center[0]) and (
+                self.player2.rect[0][1] <= self.ball.center[1]) and (
+                self.ball.center[1] <= self.player2.rect[1][1]):
+            self.ball.set_velocity((-self.ball.velocity[0], self.ball.velocity[1]))
+
+        if (self.player2.rect[0][1] - ball_radius / 2 <= self.ball.center[1] <= self.player2.rect[1][
+            1] + ball_radius / 2) and (
+                # y0 <= yb <= y1
+                self.player2.rect[0][0] <= self.ball.center[0] <= self.player2.rect[1][0]):  # x0 <= xb <= x1
+            self.ball.set_velocity((self.ball.velocity[0], -self.ball.velocity[1]))
+
+    def player2_move(self):
+        xm = self.player2.center[0]
+        ym = self.player2.center[1]
+        kb = self.ball.velocity[1] / self.ball.velocity[0]
+        cb = self.ball.center[1] - kb * self.ball.center[0]
+        yn = kb * xm + cb
+        if ym > yn:
+            self.player2.set_velocity((0, -1))
+        else:
+            self.player2.set_velocity((0, 1))
+
     def check_collision_ball_and_border(self):
         hit_left = self.check_horizontal_left_collision()
         hit_right = self.check_horizontal_right_collision()
@@ -229,10 +269,8 @@ class Game(object):
 
     def check_collision_player_and_border(self, player_):
         if (self.border.coord[2][1] >= player_.rect[0][1] or
-         player_.rect[1][1] >= self.border.coord[0][1]):
+                player_.rect[1][1] >= self.border.coord[0][1]):
             player_.set_velocity((player_.velocity[0], -player_.velocity[1]))
-
-
 
     def test_draw(self):
         self.turtle.color(color_white)
@@ -276,18 +314,6 @@ class Game(object):
                 self.spawn_new_ball()
         if hit_vertical:
             self.ball.set_velocity((self.ball.velocity[0], -self.ball.velocity[1]))
-
-    def event_handler(self):
-        pass
-
-    def move_player(self):
-        pass
-
-    def update(self):
-        pass
-
-    def game_over(self):
-        pass
 
 
 if __name__ == '__main__':
